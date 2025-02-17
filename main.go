1aa2b4c83cc9751feb947e94bfdd6cd1572b886b
isro <mohammadisro2710@gmail.com>
2025-02-17 22:49:02 +0700

    feat: create new table product

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
+type Product struct {
+	ID       uint
+	Nama     uint
+	Kategori string
+	Harga    int
+}
+
 func main() {
 	connectURI := os.Getenv("DB_CONNECTION_URI")
 
@@ -28,4 +35,18 @@ func main() {
 	}
 
 	fmt.Println("Success connect DB !")
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS produk (
+		id SERIAL PRIMARY KEY,
+		nama VARCHAR(255),
+		kategori VARCHAR(50),
+		harga INT);
+	`)
+
+	if err != nil {
+		fmt.Printf("Failed to create table : %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("success create table")
 }
